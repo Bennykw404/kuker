7891a47d6fa2045ad29887afaa61427badd34134
Muhamad Syabaini <66990156+Bennykw404@users.noreply.github.com>
2025-02-23 21:16:45 +0700

    Update install.sh

--- sstp/install.sh
+++ sstp/install.sh
@@ -128,6 +128,7 @@ generate_certificates() {
         -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 
     openssl x509 -req -days 3650 -in ia.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+    mkdir -p /home/vps/public_html
     cp server.crt /home/vps/public_html/server.crt
 
     progress 70
